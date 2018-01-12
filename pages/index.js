7a498062ddcd86eb3cc1592cbf601299293ecde1
Adit <a.saxena.email@gmail.com>
2018-01-12 15:56:11 +0100

    works

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import Layout from '../components/Layout.js';
 import indexStyles from './index.scss';
 
 export default () => {
+  const foo = () => { return 33 }
   const foo_this_is_mine = () => { var x = 1; }
   const bar_this_is_mine = () => { var x = f(); }
   
   const baz_this_is_mine = () => {
     console.log('ciao');
-    
+
+    !x && foo();
+    console.log(0 && new Foo());
+
     var x = 1;
     console.log(x);
     const unused = () => {
