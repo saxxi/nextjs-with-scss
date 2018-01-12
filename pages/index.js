5b52ba81bfb64595c7943ea3023677e71bc54aa9
Adit <a.saxena.email@gmail.com>
2018-01-12 15:41:05 +0100

    improve minified code

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
 import Layout from '../components/Layout.js';
 import indexStyles from './index.scss';
 
-export default () => (
-  <Layout>
-    <h1 className="o-h1">Nextjs with SCSS</h1>
-    <style jsx>{indexStyles}</style>
-  </Layout>
-);
+export default () => {
+  const foo_this_is_mine = () => { var x = 1; }
+  const bar_this_is_mine = () => { var x = f(); }
+  
+  const baz_this_is_mine = () => {
+    console.log('ciao');
+    
+    var x = 1;
+    console.log(x);
+    const unused = () => {
+      return 5;
+    }
+  }
+  
+  return (
+    <Layout>
+      <h1 className="o-h1">Nextjs with SCSS</h1>
+      <a onClick={baz_this_is_mine} className="button is-primary">button test</a>
+      <style jsx>{indexStyles}</style>
+    </Layout>
+  )
+}
